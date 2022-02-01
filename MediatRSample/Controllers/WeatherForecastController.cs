e28f1b1f15e7c1be573925ede1b0f470b5451d15
Vincent Nyanga <vince.nyanga@hotmail.com>
2022-02-01 06:37:47 +0200

    initial commit

---
+++ MediatRSample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MediatRSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public WeatherForecastController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<WeatherForecast>>> Get()
+        {
+            var forecast = await _mediator.Send(new GetWeatherForecaseQuery());
+            return Ok(forecast);
+        }
+    }
+
+    internal class GetWeatherForecaseQuery : IRequest<IEnumerable<WeatherForecast>>
+    {
+
+    }
+
+    internal class GetWeatherForecaseQueryHandler : IRequestHandler<GetWeatherForecaseQuery, IEnumerable<WeatherForecast>>
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<IEnumerable<WeatherForecast>> Handle(GetWeatherForecaseQuery request, CancellationToken cancellationToken)
+        {
+            var rng = new Random();
+            var forecast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            });
+
+            return Task.FromResult(forecast);
+        }
+    }
+}
